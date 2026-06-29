f465119e5fbba127ea6faba966a9f699b5a5a7ed
Tangui Louedec <tangui.louedec@gmail.com>
2026-06-30 01:22:58 +0200

    FEAT: Added random colour scheme selection on reload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const gradients = [
-    ['--background-blue',   '--background-orange'],
-    ['--background-blue',   '--background-red'   ],
-    ['--background-blue',   '--background-purple'],
-    ['--background-yellow', '--background-orange'],
-    ['--background-yellow', '--background-purple'],
+    ['--background-blue', '--background-orange', '--blue'],
+    ['--background-yellow', '--background-blue', '--blue'],
+    ['--background-lavender', '--background-orange', '--purple'],
+    ['--background-red', '--background-blue', '--red']
 ];
 
 const randomIndex = Math.floor(Math.random() * gradients.length);
-const [topColor, bottomColor] = gradients[randomIndex];
+const [topColor, bottomColor, textColor] = gradients[randomIndex];
 
 document.documentElement.style.setProperty('--gradient-top',    `var(${topColor})`);
 document.documentElement.style.setProperty('--gradient-bottom', `var(${bottomColor})`);
+document.documentElement.style.setProperty('--text', `var(${textColor})`);
