4084c585959267fb235d2d45edd03078e7f999af
Tangui Louedec <tangui.louedec@gmail.com>
2026-07-02 06:15:53 +0200

    REM: Removed svgs to avoid bugged collisions

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const gradients = [
     ['--background-blue', '--background-orange', '--blue', '--svg-yellow'],
     ['--background-yellow', '--background-blue', '--blue', '--svg-pink'],
     ['--background-lavender', '--background-orange', '--purple', '--svg-yellow'],
-    ['--background-red', '--background-blue', '--red', '--svg-pink']
+    ['--background-red', '--background-blue', '--red', '--svg-white']
 ];
 
 const randomIndex = Math.floor(Math.random() * gradients.length);
@@ -64,4 +64,4 @@ const [topColor, bottomColor, textColor, svgColour] = gradients[randomIndex];
 document.documentElement.style.setProperty('--gradient-top',    `var(${topColor})`);
 document.documentElement.style.setProperty('--gradient-bottom', `var(${bottomColor})`);
 document.documentElement.style.setProperty('--text', `var(${textColor})`);
-document.documentElement.style.setProperty('--svg-colour', `var(${textColor})`);
+document.documentElement.style.setProperty('--svg-colour', `var(${svgColour})`);
